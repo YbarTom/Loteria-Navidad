77cb54f6fe75ecea325f630bc00eb803ad032517
ausias <ausias@hINFO6-23.xiam.cat>
2023-01-13 10:09:15 +0100

    Creacion Clases 2.0

---
+++ LoteriaNavidad/src/FuncionesUtilidades.java
@@ -0,0 +1,33 @@
+
+import static loterianavidad.LoteriaNavidad.scan;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ausias
+ */
+public class FuncionesUtilidades {
+    /**
+     * Funcion Comprobar si el numero es un entero
+     * @param numero 
+     */
+    public static void Entero(int numero) {
+
+        boolean correcte = false;
+
+        do {
+            if (scan.hasNextInt()) {
+                numero = scan.nextInt();
+                correcte = true;
+            } else {
+                scan.next();
+                System.out.println("El caracter introduit no es un numero,si us plau introduiu un numero: ");
+
+            }
+        } while (correcte == false);
+    }
+}
