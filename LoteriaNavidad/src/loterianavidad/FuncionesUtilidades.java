7ab067a7a7d0a2d80dc6bdf16113ccdc1a30036b
tom <ausias@hINFO6-23.xiam.cat>
2023-01-20 08:43:31 +0100

    Intento de CountingSort

--- LoteriaNavidad/src/loterianavidad/FuncionesUtilidades.java
+++ LoteriaNavidad/src/loterianavidad/FuncionesUtilidades.java
@@ -1,21 +1,19 @@
 package loterianavidad;
 
-
 import static loterianavidad.LoteriaNavidad.scan;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import static loterianavidad.LoteriaNavidad.TotPrems;
 
-/**
- *
- * @author ausias
- */
 public class FuncionesUtilidades {
+
     /**
      * Funcion Comprobar si el numero es un entero
-     * @param numero 
+     *
+     * @param numero
      */
     public static void Entero(int numero) {
 
@@ -31,5 +29,43 @@ public class FuncionesUtilidades {
 
             }
         } while (correcte == false);
+    }
+
+    public static void CountingSort(LoteriaNavidad.NumPremiat[] vector) {
+
+        LoteriaNavidad.NumPremiat[] output = new LoteriaNavidad.NumPremiat[TotPrems + 1];
+
+        int max = vector[0].premi;
+        
+        for (int i = 1; i < TotPrems; i++) {
+            if (vector[i].premi > max) {
+                max = vector[i].premi;
+            }
+        }
+        
+        int[] count=new int [max+1];
+        
+        for(int i=0;i<max;++i){
+            count[i]=0;
+        }
+        
+        for(int i=0;i<TotPrems;i++){
+            count[vector[i].premi]++;
+        }
+        
+        for(int i=1;i<=max;i++){
+            count[i] += count[i-1];
+        }
+        
+        for(int i=TotPrems-1;i>=0;i--){
+            output[i]=new LoteriaNavidad.NumPremiat();
+            output[count[vector[i].premi]-1].premi=vector[i].premi;
+            count[vector[i].premi]--;
+        }
+        
+        for(int i=0;i<TotPrems;i++){
+            vector[i].premi = output[i].premi;
+        }
+
     }
 }
