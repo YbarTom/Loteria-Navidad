284b16b9984d6a6110677e3c3310a8303e3a8c46
tom <ausias@hINFO6-23.xiam.cat>
2023-01-27 08:13:09 +0100

    Funciones movidas a sorteo

--- LoteriaNavidad/src/loterianavidad/FuncionesSorteo.java
+++ LoteriaNavidad/src/loterianavidad/FuncionesSorteo.java
@@ -4,10 +4,97 @@
  */
 package loterianavidad;
 
+import static loterianavidad.LoteriaNavidad.PRIMERPREMI;
+import static loterianavidad.LoteriaNavidad.QUARTPREMI;
+import static loterianavidad.LoteriaNavidad.SEGONPREMI;
+import static loterianavidad.LoteriaNavidad.TERCERPREMI;
+import static loterianavidad.LoteriaNavidad.TOTNUMS;
+import static loterianavidad.LoteriaNavidad.TOTPREMS;
+
 /**
  *
  * @author ausias
  */
 public class FuncionesSorteo {
     
+    public static void GeneradorNumeros(LoteriaNavidad.NumPremiat[] vector) {
+        int contador = 0;
+        int numeroAleatorio = (int) (Math.random() * (TOTNUMS + 1));
+
+        while (contador < TOTPREMS) {
+            if (!RepetidosNumeros(numeroAleatorio, contador, vector)) {
+                vector[contador] = new LoteriaNavidad.NumPremiat();
+                vector[contador].numero = numeroAleatorio;
+                ++contador;
+            }
+            numeroAleatorio = (int) (Math.random() * (TOTNUMS + 1));
+        }
+
+    }
+
+    public static void GeneradorPremis(LoteriaNavidad.NumPremiat[] vector, int[] premis) {
+        int contador = 0;
+        int PremioAleatorio = (int) (Math.random() * (TOTPREMS + 1));
+
+        while (contador < TOTPREMS) {
+            if (!RepetidosPremis(PremioAleatorio, contador, vector)) {
+                vector[contador].premi = PremioAleatorio;
+                ++contador;
+            }
+            PremioAleatorio = (int) (Math.random() * (TOTPREMS + 1));
+        }
+        AssignarPremis(vector, premis);
+    }
+
+    public static boolean RepetidosNumeros(int n, int contador, LoteriaNavidad.NumPremiat[] vector) {
+        boolean comprovacio = false;
+
+        for (int i = 0; i <= contador && !comprovacio; i++) {
+            if (vector[i] != null && n == vector[i].numero) {
+                comprovacio = true;
+            }
+        }
+
+        return comprovacio;
+
+    }
+
+    public static boolean RepetidosPremis(int n, int contador, LoteriaNavidad.NumPremiat[] vector) {
+        boolean comprovacio = false;
+
+        for (int i = 0; i < contador && !comprovacio; i++) {
+            if (n == vector[i].premi) {
+                comprovacio = true;
+            }
+        }
+
+        return comprovacio;
+    }
+
+    public static void AssignarPremis(LoteriaNavidad.NumPremiat[] vector, int[] premisGrans) {
+
+        for (int i = 0; i < TOTPREMS; i++) {
+            if (vector[i].premi == 0) {
+                premisGrans[0] = vector[i].numero;
+                vector[i].premi = PRIMERPREMI;
+            } else if (vector[i].premi == 1) {
+                premisGrans[1] = vector[i].numero;
+                vector[i].premi = SEGONPREMI;
+            } else if (vector[i].premi == 2) {
+                premisGrans[2] = vector[i].numero;
+                vector[i].premi = TERCERPREMI;
+            } else if (vector[i].premi == 3 || vector[i].premi == 4) {
+                if (vector[i].premi == 3) {
+                    premisGrans[3] = vector[i].numero;
+                } else {
+                    premisGrans[4] = vector[i].numero;
+                }
+                vector[i].premi = QUARTPREMI;
+            } else if (vector[i].premi >= 5 && vector[i].premi <= 12) {
+                vector[i].premi = 60000;
+            } else if (vector[i].premi >= 13 && vector[i].premi <= (TOTPREMS-3)) {
+                vector[i].premi = 1000;
+            }
+        }
+    }
 }
---
+++ LoteriaNavidad/test/loterianavidad/LoteriaNavidadTest.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package loterianavidad;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ausias
+ */
+public class LoteriaNavidadTest {
+    
+    public LoteriaNavidadTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of GeneradorNumeros method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testGeneradorNumeros() {
+        System.out.println("GeneradorNumeros");
+        LoteriaNavidad.NumPremiat[] vector = null;
+        LoteriaNavidad.GeneradorNumeros(vector);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of GeneradorPremis method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testGeneradorPremis() {
+        System.out.println("GeneradorPremis");
+        LoteriaNavidad.NumPremiat[] vector = null;
+        int[] premis = null;
+        LoteriaNavidad.GeneradorPremis(vector, premis);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of RepetidosNumeros method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testRepetidosNumeros() {
+        System.out.println("RepetidosNumeros");
+        int n = 0;
+        int contador = 0;
+        LoteriaNavidad.NumPremiat[] vector = null;
+        boolean expResult = false;
+        boolean result = LoteriaNavidad.RepetidosNumeros(n, contador, vector);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of RepetidosPremis method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testRepetidosPremis() {
+        System.out.println("RepetidosPremis");
+        int n = 0;
+        int contador = 0;
+        LoteriaNavidad.NumPremiat[] vector = null;
+        boolean expResult = false;
+        boolean result = LoteriaNavidad.RepetidosPremis(n, contador, vector);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of AssignarPremis method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testAssignarPremis() {
+        System.out.println("AssignarPremis");
+        LoteriaNavidad.NumPremiat[] vector = null;
+        int[] premisGrans = null;
+        LoteriaNavidad.AssignarPremis(vector, premisGrans);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class LoteriaNavidad.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        LoteriaNavidad.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
