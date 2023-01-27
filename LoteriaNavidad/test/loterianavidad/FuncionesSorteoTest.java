a590d9f7461900c07a64f11e671469d692e0f9bd
tom <ausias@hINFO6-23.xiam.cat>
2023-01-27 09:45:42 +0100

    nombre test sorteo

--- LoteriaNavidad/test/loterianavidad/FuncionesSorteoTest.java
+++ LoteriaNavidad/test/loterianavidad/FuncionesSorteoTest.java
@@ -15,9 +15,9 @@ import static org.junit.Assert.*;
  *
  * @author ausias
  */
-public class LoteriaNavidadTest {
+public class FuncionesSorteoTest {
     
-    public LoteriaNavidadTest() {
+    public FuncionesSorteoTest() {
     }
     
     @BeforeClass
