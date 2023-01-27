86e2d9b38b460496bf3e6141dfb0ef5d24e45bda
tom <ausias@hINFO6-23.xiam.cat>
2023-01-27 09:49:21 +0100

    funciones sorteo bien

--- LoteriaNavidad/src/loterianavidad/LoteriaNavidad.java
+++ LoteriaNavidad/src/loterianavidad/LoteriaNavidad.java
@@ -5,6 +5,8 @@
 package loterianavidad;
 
 import java.util.Scanner;
+import static loterianavidad.FuncionesSorteo.GeneradorNumeros;
+import static loterianavidad.FuncionesSorteo.GeneradorPremis;
 
 public class LoteriaNavidad {
 
@@ -26,87 +28,7 @@ public class LoteriaNavidad {
         int premi;
     }
 
-    public static void GeneradorNumeros(NumPremiat[] vector) {
-        int contador = 0;
-        int numeroAleatorio = (int) (Math.random() * (TOTNUMS + 1));
-
-        while (contador < TOTPREMS) {
-            if (!RepetidosNumeros(numeroAleatorio, contador, vector)) {
-                vector[contador] = new NumPremiat();
-                vector[contador].numero = numeroAleatorio;
-                ++contador;
-            }
-            numeroAleatorio = (int) (Math.random() * (TOTNUMS + 1));
-        }
-
-    }
-
-    public static void GeneradorPremis(NumPremiat[] vector, int[] premis) {
-        int contador = 0;
-        int PremioAleatorio = (int) (Math.random() * (TOTPREMS + 1));
-
-        while (contador < TOTPREMS) {
-            if (!RepetidosPremis(PremioAleatorio, contador, vector)) {
-                vector[contador].premi = PremioAleatorio;
-                ++contador;
-            }
-            PremioAleatorio = (int) (Math.random() * (TOTPREMS + 1));
-        }
-        AssignarPremis(vector, premis);
-    }
-
-    public static boolean RepetidosNumeros(int n, int contador, NumPremiat[] vector) {
-        boolean comprovacio = false;
-
-        for (int i = 0; i <= contador && !comprovacio; i++) {
-            if (vector[i] != null && n == vector[i].numero) {
-                comprovacio = true;
-            }
-        }
-
-        return comprovacio;
-
-    }
-
-    public static boolean RepetidosPremis(int n, int contador, NumPremiat[] vector) {
-        boolean comprovacio = false;
-
-        for (int i = 0; i < contador && !comprovacio; i++) {
-            if (n == vector[i].premi) {
-                comprovacio = true;
-            }
-        }
-
-        return comprovacio;
-    }
-
-    public static void AssignarPremis(NumPremiat[] vector, int[] premisGrans) {
-
-        for (int i = 0; i < TOTPREMS; i++) {
-            if (vector[i].premi == 0) {
-                premisGrans[0] = vector[i].numero;
-                vector[i].premi = PRIMERPREMI;
-            } else if (vector[i].premi == 1) {
-                premisGrans[1] = vector[i].numero;
-                vector[i].premi = SEGONPREMI;
-            } else if (vector[i].premi == 2) {
-                premisGrans[2] = vector[i].numero;
-                vector[i].premi = TERCERPREMI;
-            } else if (vector[i].premi == 3 || vector[i].premi == 4) {
-                if (vector[i].premi == 3) {
-                    premisGrans[3] = vector[i].numero;
-                } else {
-                    premisGrans[4] = vector[i].numero;
-                }
-                vector[i].premi = QUARTPREMI;
-            } else if (vector[i].premi >= 5 && vector[i].premi <= 12) {
-                vector[i].premi = 60000;
-            } else if (vector[i].premi >= 13 && vector[i].premi <= (TOTPREMS-3)) {
-                vector[i].premi = 1000;
-            }
-        }
-    }
-
+    
     public static void main(String[] args) {
 
         NumPremiat VectorPremi[] = new NumPremiat[1807];
